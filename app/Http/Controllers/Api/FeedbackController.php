b653aa3d61a672f60897e1a4355f385ecb9f0110
dawrandev <dawrandev@gmail.com>
2026-05-25 17:20:04 +0500

    added log in feedbackcontroller

--- app/Http/Controllers/Api/FeedbackController.php
+++ app/Http/Controllers/Api/FeedbackController.php
@@ -13,80 +13,85 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Log;
+
 class FeedbackController extends Controller
 {
-    public function questions($slug){
-        $questions=ReviewQuestion::whereHas('business', function($q) use($slug){
+    public function questions($slug)
+    {
+        $questions = ReviewQuestion::whereHas('business', function ($q) use ($slug) {
             $q->where('slug', '=', $slug);
         })
-        ->when(
-            Business::where('slug', $slug)->value('type') === 'single_choice',
-            fn ($query) => $query->with('questionOptions')
-        )->get();
+            ->when(
+                Business::where('slug', $slug)->value('type') === 'single_choice',
+                fn($query) => $query->with('questionOptions')
+            )->get();
         return $this->responsePagination($questions, QuestionResource::collection($questions));
     }
-    public function business(Request $request){
-        $query=Business::where('status',true);
-        if($request->has('slug')){
-            $slug=$request->slug;
-            $query=$query->where('slug','=',$slug);
+    public function business(Request $request)
+    {
+        $query = Business::where('status', true);
+        if ($request->has('slug')) {
+            $slug = $request->slug;
+            $query = $query->where('slug', '=', $slug);
         }
-        $business=$query->get();
-        
+        $business = $query->get();
+
         return $this->responsePagination($business, BusinessResource::collection($business));
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
+        Log::info('Request data:', $request->all());
         $business = Business::findOrFail($request->business_id);
         $rules = [
             'business_id' => 'required|exists:businesses,id',
             'feedback' => 'required|array',
             'feedback.*.question_id' => 'required|exists:review_questions,id',
         ];
-        
+
         if ($business->type === 'single_choice') {
             $rules['feedback.*.question_option_id'] = 'required|exists:question_options,id';
         }
-        
+
         if ($business->type === 'rating') {
             $rules['feedback.*.rating'] = 'required|integer|min:1|max:5';
         }
-        
-        
+
+
         $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
         $token = $business->token;
         $chatId = $business->chat_id;
-        $feedback=Feedback::create([
-            'owner_id'=>$business->owner_id,
-            'business_id'=>$request->business_id,
-            'comment'=>$request->comment
+        $feedback = Feedback::create([
+            'owner_id' => $business->owner_id,
+            'business_id' => $request->business_id,
+            'comment' => $request->comment
         ]);
         foreach ($request->feedback as $key => $feed) {
             FeedbackDetail::create([
-                'feedback_id'=>$feedback->id,
-                'business_id'=>$request->business_id,
-                'review_question_id'=>$feed['question_id'],
-                'question_option_id'=>$business->type == 'rating' ? null : $feed['question_option_id'],
-                'rating'=>$business->type == 'rating' ? $feed['rating'] : null
+                'feedback_id' => $feedback->id,
+                'business_id' => $request->business_id,
+                'review_question_id' => $feed['question_id'],
+                'question_option_id' => $business->type == 'rating' ? null : $feed['question_option_id'],
+                'rating' => $business->type == 'rating' ? $feed['rating'] : null
             ]);
         }
 
-        
+
         // Custom message format
         $message = "*Новый отзыв⭐️*\n";
         foreach ($feedback->feedbackDetails as $key => $detail) {
-            if($business->type == 'rating'){
+            if ($business->type == 'rating') {
                 $stars = str_repeat("⭐", $detail->rating);  // Ratingga qarab yulduzlarni ko‘paytirish
-                $question=$detail->reviewQuestion->question;
+                $question = $detail->reviewQuestion->question;
                 // $message .= "*Ответ: *" . $detail->rating." ".$stars."\n";
-                $message .= "*$question: *" . $detail->rating."-".$stars."\n";
-            }else{
-                $question=$detail->reviewQuestion->question;
-                $text=$detail->QuestionOption->text;
+                $message .= "*$question: *" . $detail->rating . "-" . $stars . "\n";
+            } else {
+                $question = $detail->reviewQuestion->question;
+                $text = $detail->QuestionOption->text;
                 // $message .= "*Ответ: *" . $detail->rating." ".$stars."\n";
-                $message .= "*$question: *" . $text."\n";
+                $message .= "*$question: *" . $text . "\n";
             }
         }
         $message .= "*Пожелания: *" . $feedback->comment;
@@ -103,6 +108,6 @@ class FeedbackController extends Controller
             Log::error('SMS yuborilmadi', ['feedback_id' => $feedback->id, 'status' => $response->status()]);
         }
 
-        return response()->json(['message' => 'Feedback success'],200);
+        return response()->json(['message' => 'Feedback success'], 200);
     }
 }
